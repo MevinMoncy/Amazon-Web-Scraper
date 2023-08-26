3f73227e84d57c0eb855bc950a64fca8e8bd75c0
MevinMoncy <mevinab1@gmail.com>
2023-08-26 15:19:27 -0600

    imported selenium and defined the path

--- Amazon_WebScrapper.py
+++ Amazon_WebScrapper.py
@@ -1,2 +1,11 @@
 import csv
 from bs4 import BeautifulSoup
+
+### Startup the webdriver
+from selenium import webdriver #webdriver is the one driving the action;links up w/ the browser, and performs actions
+from selenium.webdriver.common.keys import Keys #lets you to type something in the searchbar and enter and see all the search results
+
+PATH = r"C:\Program Files (x86)\chromedriver.exe"
+
+#The web browser we want to use is chrome, and the web driver web driver for this browser is located at this PATH
+driver = webdriver.Chrome(PATH)
